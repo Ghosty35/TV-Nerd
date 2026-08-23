3e7cc13eed269ff2440914d9d28c14b0c573ec40
Ghosty35 <Ghosty35@users.noreply.github.com>
2026-08-23 16:11:06 +0200

    Add Release calendar to the top-bar More menu

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tvnerd-shell-v24';
+const CACHE_NAME = 'tvnerd-shell-v25';
 const SHELL = ['/', './legacy.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './favicon.svg'];
 
 self.addEventListener('install', event => {
